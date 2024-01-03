e60edb69cc40befd934cb3160d78d64af10f766e
MuriloMacedoSilva <152343609+MuriloMacedoSilva@users.noreply.github.com>
2024-01-02 23:45:20 -0300

    a

---
+++ projeto-mae/reveal.js
@@ -0,0 +1,38 @@
+window.revelar = ScrollReveal({reset:true})
+
+// TOPO DO SITE
+
+revelar.reveal('.efeito-main-topo',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+// containers
+
+revelar.reveal('.experiencia-container1',
+{
+    duration: 2000,
+    distance: '90px',
+})
+
+revelar.reveal('.experiencia-container2',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 250
+})
+
+revelar.reveal('.experiencia-container3',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
