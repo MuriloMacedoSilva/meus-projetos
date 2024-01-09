6bb6abee8199879cbbe2fc14482d930207a5b2a8
MuriloMacedoSilva <152343609+MuriloMacedoSilva@users.noreply.github.com>
2024-01-09 13:32:25 -0300

    Update slide.js

--- projeto-slide/slide-games-base/slide.js
+++ projeto-slide/slide-games-base/slide.js
@@ -7,23 +7,31 @@ const navNextButton = document.querySelector('[data-slide="nav-next-button"]')
 const controlsWrapper = document.querySelector('[data-slide="controls-wrapper"]')
 const slideItems = document.querySelectorAll('[data-slide="item"]')
 const controlButtons = document.querySelectorAll('[data-slide="control-button"]')
+let startingPoint = 0
+let savedPosition = 0
+let currentPoint = 0
 
-function onMouseDown (event) {
+function onMouseDown(event) {
     const slideItem = event.currentTarget
+    startingPoint = event.clientX
+    currentPoint = startingPoint - savedPosition
     slideItem.addEventListener('mousemove', onMouseMove)
-    console.log('pixel do mousedown', event.clientX)
-    console.log('apertei o botão do mouse')
+    console.log('ponto de partida', startingPoint)
+    //console.log('apertei o botão do mouse')
 }
 
 function onMouseMove(event) {
-    console.log('movimentando o botão do mouse')
-    console.log('pixel do mousemove', event.clientX)
+    const movement = event.clientX - startingPoint
+    const position = event.clientX - currentPoint
+    console.log('pixel do mousemove', event.clientX, ' - ', 'ponto de partida', startingPoint, ' = ', movement)
+    slideList.style.transform = 'translateX('+position+'px)'
+    savedPosition = position
 }
 
 function onMouseUp(event) {
     const slideItem = event.currentTarget
     slideItem.removeEventListener('mousemove', onMouseMove)
-    console.log('soltei o botão do mouse')
+    //console.log('soltei o botão do mouse')
 }
 
 slideItems.forEach(function(slideItem, index) {
