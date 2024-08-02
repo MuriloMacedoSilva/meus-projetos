03dffac9bb9e4c85bf1fb1280557304007631707
MuriloMacedoSilva <152343609+MuriloMacedoSilva@users.noreply.github.com>
2024-08-02 10:52:27 -0300

    .

---
+++ meu-portfolio/reveal.js
@@ -0,0 +1,45 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+// TOPO DO SITE 
+
+revelar.reveal('.efeito-img-topo',{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-txt-topo',{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+// SOBRE
+
+revelar.reveal('.titulo1',{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.img2',{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.img3',{
+    duration: 2000,
+    distance: '50px'
+})
+
+revelar.reveal('.img4',{
+    duration: 2000,
+    distance: '50px',
+    delay: 500
+})
+
+revelar.reveal('.img5',{
+    duration: 1000,
+    distance: '20px'
+})
+
