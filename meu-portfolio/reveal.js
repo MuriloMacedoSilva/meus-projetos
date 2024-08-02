240eaa0c65d0bdc7f53254ce263c7d4b6cc6aeb7
MuriloMacedoSilva <152343609+MuriloMacedoSilva@users.noreply.github.com>
2024-08-02 11:07:29 -0300

    Update reveal.js

--- meu-portfolio/reveal.js
+++ meu-portfolio/reveal.js
@@ -4,12 +4,12 @@ window.revelar = ScrollReveal({reset:true})
 // TOPO DO SITE 
 
 revelar.reveal('.efeito-img-topo',{
-    duration: 2000,
+    duration: 1000,
     distance: '40px'
 })
 
 revelar.reveal('.efeito-txt-topo',{
-    duration: 2000,
+    duration: 1000,
     distance: '40px',
     delay: 500
 })
@@ -17,23 +17,23 @@ revelar.reveal('.efeito-txt-topo',{
 // SOBRE
 
 revelar.reveal('.titulo1',{
-    duration: 2000,
+    duration: 1000,
     distance: '40px'
 })
 
 revelar.reveal('.img2',{
-    duration: 2000,
+    duration: 1000,
     distance: '40px',
     delay: 500
 })
 
 revelar.reveal('.img3',{
-    duration: 2000,
+    duration: 1000,
     distance: '50px'
 })
 
 revelar.reveal('.img4',{
-    duration: 2000,
+    duration: 1000,
     distance: '40px',
     delay: 500
 })
@@ -44,23 +44,23 @@ revelar.reveal('.img5',{
 })
 
 revelar.reveal('.efeito1',{
-    duration: 2000,
+    duration: 1000,
     distance: '40px'
 })
 
 revelar.reveal('.efeito2',{
-    duration: 2000,
+    duration: 1000,
     distance: '40px',
     delay: 400
 })
 
 revelar.reveal('.efeito3',{
-    duration: 2000,
+    duration: 1000,
     distance: '40px',
     delay: 300
 })
 
 revelar.reveal('.baixo',{
-    duration: 2000,
+    duration: 1000,
     distance: '40px'
 })
